ae9a0121b833b4ebaf49f25103b154cef8a1d3ba
fynn emmanuel junior <fynn.emmanuel.junior@gmai.com>
2024-10-26 16:20:46 +0000

    updated affirmation practice screen

--- app/(tabs)/affirmations/[itemId].tsx
+++ app/(tabs)/affirmations/[itemId].tsx
@@ -1,8 +1,11 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,ImageBackground, Pressable } from 'react-native';
 import React, { useEffect, useState } from 'react';
-import { useLocalSearchParams } from 'expo-router';
+import { useLocalSearchParams,router } from 'expo-router';
 import { GalleryPreviewData } from '@/constants/models/AffirmationCategory';
 import AFFIRMATION_GALLERY from '@/constants/affirmation-gallery';
+import AppGradient from '@/components/AppGradient';
+import AntDesign from '@expo/vector-icons/AntDesign';
+import { StatusBar } from 'expo-status-bar';
 
 const AffirmationPractice = () => {
   const { itemId } = useLocalSearchParams();
@@ -25,8 +28,22 @@ const AffirmationPractice = () => {
   console.log(affirmation);
 
   return (
-    <View>
-      <Text>{itemId}</Text>
+    <View className='flex-1'>
+      <ImageBackground 
+        source={affirmation?.image}
+        resizeMode='cover'
+        className='flex-1'
+      >
+        <AppGradient colors={['rgba(0,0,0,0.3)','rgba(0,0,0,0.9)']}>
+          <Pressable 
+            onPress={() => router.back()}
+            className='absolute top-16 left-6'
+          >
+            <AntDesign name="leftcircleo" size={40} color="white" />
+          </Pressable>
+        </AppGradient>
+      </ImageBackground>
+      <StatusBar style='light'/>
     </View>
   );
 };
