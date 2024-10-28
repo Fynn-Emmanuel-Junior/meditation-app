a63ca649a19f4084224caefc28e413a759825520
fynn emmanuel junior <fynn.emmanuel.junior@gmail.com>
2024-10-28 10:26:40 +0000

    update meditate screen ui

--- app/(tabs)/meditate.tsx
+++ app/(tabs)/meditate.tsx
@@ -1,7 +1,8 @@
-import { StyleSheet, Text, View,ImageBackground } from 'react-native'
+import { StyleSheet, Text, View,ImageBackground, Pressable } from 'react-native'
 import React from 'react'
 import meditationImages from '@/constants/meditation-images'
 import AppGradient from '@/components/AppGradient'
+import AntDesign from '@expo/vector-icons/AntDesign';
 
 const meditate = () => {
   return (
@@ -11,7 +12,13 @@ const meditate = () => {
             resizeMode='cover'
             className='flex-1'
         >
+            <AppGradient
+                colors={['transparent','rgba(0,0,0,0.8)']}
+            >
+                <Pressable>
 
+                </Pressable>
+            </AppGradient>
         </ImageBackground>
     </View>
   )
